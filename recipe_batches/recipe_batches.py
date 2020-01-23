748a23b9206211d1395737a1a9f8ed5208221701
ryanZiegenfus <thewebcoderz@gmail.com>
2020-01-23 16:32:40 -0500

    recipes

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,8 +3,19 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  if len(recipe) <= len(ingredients):
+    batch_arr = []
+    for key in recipe and ingredients:
+      batches = ingredients[key]//recipe[key]
+      batch_arr.append(batches)
+      max_batches = batch_arr[0]
+    for b in range(len(batch_arr)-1):
+      if batch_arr[b] < max_batches:
+        max_batches = batch_arr[b]
+    return max_batches
+  return 0
 
+print(recipe_batches({ 'milk': 100, 'flour': 4, 'sugar': 10, 'butter': 5 }, { 'milk': 1288, 'flour': 9, 'sugar': 95 }))
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
