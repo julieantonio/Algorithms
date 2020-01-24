b49b0089816a0db1be3dc3d2863e319822af0d2f
ryanZiegenfus <thewebcoderz@gmail.com>
2020-01-24 00:46:06 -0500

    rps

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,9 +2,29 @@
 
 import sys
 
-def rock_paper_scissors(n):
-  pass 
+# def rock_paper_scissors(n):
+#   rpsList = [['rock'], ['paper'], ['scissors']]
+#   if n == 0:
+#     return rpsList
+#   else:
+#     rps2 = rpsList + ['rock']
+#     rps2 = rpsList + ['paper']
+#     rps2 = rpsList + ['scissors']
+#     return rock_paper_scissors(append(rock)), rock_paper_scissors(append(scissors)), rock_paper_scissors(append(paper))
 
+def rock_paper_scissors(n):
+  results= []
+  outcomes = ['rock', 'paper', 'scissors']
+  def add_player(n, moves=[]):
+    if n == 0:
+      return results.append(moves)
+    for x in range(3):
+      #moves.append(outcomes[x])
+      add_player(n - 1, moves + [outcomes[x]])
+  add_player(n)
+  return results
+
+print(rock_paper_scissors(2))
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
